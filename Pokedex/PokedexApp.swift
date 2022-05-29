3d9631e97e137632fbed32642cbce45dd56de2e1
Eliúde Vemba <sweeteliude@gmail.com>
2022-05-29 04:16:49 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Eliude Vemba on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
